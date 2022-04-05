52181e8080d90a6a6059ab0c583602f928f20996
Lucas Medeiros <lucasmedeiros1994@gmail.com>
2022-04-05 19:49:29 -0300

    chore: add and configure phpunit into project

---
+++ tests/Unit/TestUnitTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Unit;
+
+use Core\Test;
+use PHPUnit\Framework\TestCase;
+
+class TestUnitTest extends TestCase
+{
+
+    public function test_call_method_foo()
+    {
+        $test = new Test();
+        $response = $test->foo();
+
+        $this->assertEquals('123', $response);
+    }
+
+}
